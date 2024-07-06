ac7f973fe6ca7c898f4e917936f06d015a808518
Matias Godoy <79279761+matigodoy@users.noreply.github.com>
2024-07-06 18:35:10 -0300

    Se inicializa el nuevo proyecto en NET

---
+++ Final/DOO/Home.cs
@@ -0,0 +1,15 @@
+namespace DOO
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
